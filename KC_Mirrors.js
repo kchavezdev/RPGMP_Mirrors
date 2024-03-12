b7166baa43bab146022a4158c72d72cbeb24ab2d
K. Chavez <kchavez.dev@gmail.com>
2024-03-12 00:48:06 -0700

    Edit tryParseParameter helper func
    yeah let's not use an eval here

--- KC_Mirrors.js
+++ KC_Mirrors.js
@@ -897,6 +897,42 @@ KCDev.Mirrors.parseMetaValues = function (reflectableObj, target, defaults, isAc
     }
     else {
 
+        /**
+         * @param {string} param
+         * @returns {any} 
+         */
+        function tryParseParameter(param) {
+
+            if (typeof param !== 'string') return param;
+
+            // first try parsing as an object
+            try {
+                return JsonEx.parse(param);
+            } catch (error) {
+
+            }
+
+            // this ensures param JUST has numbers in it
+            // Number('') returns 0, which is undesirable
+            // parseFloat('123abc') returns 123, which is also not wanted
+            // so we have to use both to ensure whitespace is not parsed and characters are not ignored
+            const num = Number(param)
+            if (num === parseFloat(param)) {
+                return num;
+            }
+
+            if (param === 'true') {
+                return true;
+            }
+
+            if (param === 'false') {
+                return false;
+            }
+
+            // if those failed, it's probably a string so leave alone
+            return param;
+        }
+
         const script = document.currentScript.src.split("/").pop().replace(/\.js$/, "");
 
         const /** @type {KCDev.Mirrors.PluginParams} */ parameters = PluginManager.parameters(script);
