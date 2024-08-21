25e28c8348e98b2263f6023a5705286e368361d8
K. Chavez <kchavez.dev@gmail.com>
2024-08-20 22:43:29 -0700

    document plugin parameters

--- src/KC_Mirrors.ts
+++ src/KC_Mirrors.ts
@@ -1,1 +1,21 @@
+namespace KCDev {
+    export namespace Mirrors {
+        export interface ICharacterDefault {
+            isWallEnabled: boolean
+            isFloorEnabled: boolean
+        }
+        export enum WallReflectMode {
+            PERSPECTIVE = 1,
+            EVENT = 2
+        }
+        export class PluginParameters {
+            static zValue: number = -1
+            static wallReflectType: WallReflectMode = WallReflectMode.PERSPECTIVE
+            static wallReflectVar: number = 0
+            static actorDefault: ICharacterDefault = {isWallEnabled: true, isFloorEnabled: true}
+            static eventDefault: ICharacterDefault = {isWallEnabled: false, isFloorEnabled: false}
+            static isPerspectiveYsortEnabled: boolean = false
+        }
+    }
+}
 
