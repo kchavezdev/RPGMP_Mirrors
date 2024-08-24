1b1df1e4e902c3e539efcfa8c96361870e872b51
K. Chavez <kchavez.dev@gmail.com>
2024-08-24 11:11:10 -0700

    Change reflection sprite blending handling
    Now only set if the properties changed

--- src/KC_Mirrors.ts
+++ src/KC_Mirrors.ts
@@ -310,8 +310,8 @@ Sprite_Character.prototype.updateReflectionCommon = function (this: Sprite_Chara
     reflectSprite.rotation = this.rotation + charReflect.rotation;
     reflectSprite.x = this.x + charReflect.offset.x;
     reflectSprite.y = this.y + charReflect.offset.y;
-    reflectSprite._blendColor = this._blendColor;
-    reflectSprite._blendMode = this._blendMode;
+    setPropIfNonMatching(reflectSprite, this, '_blendColor');
+    setPropIfNonMatching(reflectSprite, this, 'blendMode');
     reflectSprite.scale.x = this.scale.x;
     reflectSprite.scale.y = this.scale.y;
 
