4d49f68093335beff0299634a0f7458d8cd4f10d
K. Chavez <kchavez.dev@gmail.com>
2024-08-26 15:21:30 -0700

    Add region plugin header parameters

--- src/plugin_header.ts
+++ src/plugin_header.ts
@@ -12,4 +12,28 @@
  * @help
  * KC_Mirrors
  * 
- */ 
+ * @param regionsParent
+ * @text Regions
+ * 
+ * @param wallRegions
+ * @parent regionsParent
+ * @text Wall Region IDs
+ * @desc A wall reflection sprite will be drawn on tiles with any of these region IDs.
+ * @type number[]
+ * @default []
+ * 
+ * @param noReflectRegionsFloor
+ * @parent regionsParent
+ * @text Floor Exclusion regions
+ * @desc Characters on tiles with these region IDs will not have floor reflections.
+ * @type number[]
+ * @default []
+ * 
+ * @param noReflectRegionsWall
+ * @parent regionsParent
+ * @text Wall Exclusion regions
+ * @desc Characters on tiles with these region IDs will not have wall reflections.
+ * @type number[]
+ * @default []
+ * 
+ */
